91b3e7334dbab70baf9104e70faac4b3abfa5743
hiepp7843720 <duyhiep063@gmail.com>
2026-02-17 00:44:21 +0700

    Update data.js

--- data.js
+++ data.js
@@ -3,12 +3,12 @@ const data_name = [
         ma_ten: "cong",
         name: "Công",
         message: "Hello, I'm Công",
-        img: "image/dvp_vnu.jpg"
+        img: "image/1.jpg"
     },
     {
         ma_ten: "thanh",
         name: "Thanh",
         message: "Hello, I'm Thanh",
-        img: "image/dvp_vnu.jpg"
+        img: "image/1.jpg"
     }
 ]
